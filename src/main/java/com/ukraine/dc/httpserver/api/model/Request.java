f9a9aa3d1023154aad062800c627224f86b4c15e
david <chursa99192@gmail.com>
2022-10-25 03:38:53 +0300

    [http-server] - update model layer via adding lombok annotations

--- src/main/java/com/ukraine/dc/httpserver/api/model/Request.java
+++ src/main/java/com/ukraine/dc/httpserver/api/model/Request.java
@@ -1,42 +1,18 @@
 package com.ukraine.dc.httpserver.api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Map;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Request {
     private String requestUri;
     private Map<String, String> headers;
     private HttpMethod httpMethod;
-
-    public Request() {
-    }
-
-    public Request(String requestUri, Map<String, String> headers, HttpMethod httpMethod) {
-        this.requestUri = requestUri;
-        this.headers = headers;
-        this.httpMethod = httpMethod;
-    }
-
-    public String getRequestUri() {
-        return requestUri;
-    }
-
-    public void setRequestUri(String requestUri) {
-        this.requestUri = requestUri;
-    }
-
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
-
-    public void setHeaders(Map<String, String> headers) {
-        this.headers = headers;
-    }
-
-    public HttpMethod getHttpMethod() {
-        return httpMethod;
-    }
-
-    public void setHttpMethod(HttpMethod httpMethod) {
-        this.httpMethod = httpMethod;
-    }
 }
--- src/main/java/com/ukraine/dc/httpserver/api/model/Response.java
+++ src/main/java/com/ukraine/dc/httpserver/api/model/Response.java
@@ -1,42 +1,18 @@
 package com.ukraine.dc.httpserver.api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Map;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Response {
-    private String content;
+    private byte[] content;
     private StatusCode statusCode;
     private Map<String, String> headers;
-
-    public Response() {
-    }
-
-    public Response(StatusCode statusCode, String content, Map<String, String> headers) {
-        this.statusCode = statusCode;
-        this.content = content;
-        this.headers = headers;
-    }
-
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
-
-    public void setHeaders(Map<String, String> headers) {
-        this.headers = headers;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public StatusCode getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(StatusCode statusCode) {
-        this.statusCode = statusCode;
-    }
 }
