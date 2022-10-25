752f4db500e84d1fbcf4cc7e5592e880f4851607
david <chursa99192@gmail.com>
2022-10-25 03:41:23 +0300

    [http-server] - update requestHandler, server

--- src/main/java/com/ukraine/dc/httpserver/api/Server.java
+++ src/main/java/com/ukraine/dc/httpserver/api/Server.java
@@ -1,14 +1,17 @@
 package com.ukraine.dc.httpserver.api;
 
 import com.ukraine.dc.httpserver.api.service.RequestHandler;
-import com.ukraine.dc.httpserver.api.service.parser.RequestParser;
-import com.ukraine.dc.httpserver.api.service.reader.ResourceReader;
-import com.ukraine.dc.httpserver.api.service.writer.ResponseWriter;
+import com.ukraine.dc.httpserver.api.service.parser.SocketRequestParser;
+import com.ukraine.dc.httpserver.api.service.reader.FileResourceReader;
+import com.ukraine.dc.httpserver.api.service.writer.SocketResponseWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.ServerSocket;
 
 public class Server {
+    private static final Logger log = LoggerFactory.getLogger(Server.class);
     private static final int DEFAULT_PORT = 3000;
     private String webResourcesPath;
     private int port;
@@ -22,16 +25,26 @@ public class Server {
     }
 
     public void start() {
-        var parser = new RequestParser();
-        var reader = new ResourceReader(webResourcesPath);
-        var writer = new ResponseWriter();
+        var parser = new SocketRequestParser();
+        var reader = new FileResourceReader(webResourcesPath);
+        var writer = new SocketResponseWriter();
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
                 var socket = serverSocket.accept();
-                new Thread(() -> new RequestHandler(reader, writer, parser).handle(socket)).start();
+                log.info("Server started");
+                new Thread(() -> {
+                    try (var inputStream = socket.getInputStream();
+                         var outputStream = socket.getOutputStream()) {
+                        log.info("The method handle() was invoked on RequestHandler");
+                        new RequestHandler(reader, writer, parser).handle(inputStream, outputStream);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).start();
             }
         } catch (IOException exception) {
+            log.info("Socket was closed");
             throw new RuntimeException(exception);
         }
     }
--- src/main/java/com/ukraine/dc/httpserver/api/service/RequestHandler.java
+++ src/main/java/com/ukraine/dc/httpserver/api/service/RequestHandler.java
@@ -1,58 +1,44 @@
 package com.ukraine.dc.httpserver.api.service;
 
-import com.ukraine.dc.httpserver.api.exception.BadRequestException;
-import com.ukraine.dc.httpserver.api.exception.InternalServerErrorException;
-import com.ukraine.dc.httpserver.api.exception.ResourceNotFoundException;
+import com.ukraine.dc.httpserver.api.exception.HttpException;
+import com.ukraine.dc.httpserver.api.model.HttpMethod;
 import com.ukraine.dc.httpserver.api.model.Response;
 import com.ukraine.dc.httpserver.api.model.StatusCode;
-import com.ukraine.dc.httpserver.api.service.parser.ItemParser;
-import com.ukraine.dc.httpserver.api.service.reader.ItemReader;
-import com.ukraine.dc.httpserver.api.service.writer.ItemWriter;
+import com.ukraine.dc.httpserver.api.service.parser.RequestParser;
+import com.ukraine.dc.httpserver.api.service.reader.ResourceReader;
+import com.ukraine.dc.httpserver.api.service.writer.ResponseWriter;
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.net.Socket;
 
 public class RequestHandler {
-    private final ItemReader resourceReader;
-    private final ItemWriter responseWriter;
-    private final ItemParser requestParser;
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private final ResourceReader resourceReader;
+    private final ResponseWriter responseWriter;
+    private final RequestParser requestParser;
 
-    public RequestHandler(ItemReader reader, ItemWriter writer, ItemParser parser) {
+    public RequestHandler(ResourceReader reader, ResponseWriter writer, RequestParser parser) {
         this.resourceReader = reader;
         this.responseWriter = writer;
         this.requestParser = parser;
     }
 
-    public void handle(Socket socket) {
-        try (var reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             var writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
-            Response response = new Response();
-            try {
-                var request = requestParser.parse(reader);
-                var content = resourceReader.readContent(request.getRequestUri());
-                response.setStatusCode(StatusCode.OK);
-                response.setContent(content);
-                response.setHeaders(request.getHeaders());
-                responseWriter.writeResponse(writer, response);
-            } catch (UnsupportedOperationException e) {
-                response.setStatusCode(StatusCode.METHOD_NOT_ALLOWED);
-                response.setContent(StatusCode.METHOD_NOT_ALLOWED.getMessage());
-                responseWriter.writeResponse(writer, response);
-            } catch (ResourceNotFoundException e) {
-                response.setStatusCode(StatusCode.NOT_FOUND);
-                response.setContent(StatusCode.NOT_FOUND.getMessage());
-                responseWriter.writeResponse(writer, response);
-            } catch (BadRequestException e) {
-                response.setStatusCode(StatusCode.BAD_REQUEST);
-                response.setContent(StatusCode.BAD_REQUEST.getMessage());
-                responseWriter.writeResponse(writer, response);
-            } catch (InternalServerErrorException e) {
-                response.setStatusCode(StatusCode.INTERNAL_SERVER_ERROR);
-                response.setContent(StatusCode.INTERNAL_SERVER_ERROR.getMessage());
-                responseWriter.writeResponse(writer, response);
-            }
-        } catch (IOException e) {
-            throw new InternalServerErrorException("Internal Server Error", e);
+    @SneakyThrows
+    public void handle(InputStream inputStream, OutputStream outputStream) {
+        try {
+            var request = requestParser.parse(inputStream);
+            var content = resourceReader.readContent(request);
+            responseWriter.writeResponse(outputStream, Response.builder()
+                    .content(content)
+                    .statusCode(StatusCode.OK)
+                    .headers(request.getHeaders())
+                    .build());
+            log.info("Method handle() finished successfully.");
+        } catch (HttpException e) {
+            log.info("Method handle() failed");
+            responseWriter.writeErrorResponse(outputStream, e);
         }
     }
 
