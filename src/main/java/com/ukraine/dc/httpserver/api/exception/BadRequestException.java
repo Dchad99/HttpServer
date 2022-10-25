1373ca1ac67acf12a68ad94115f106b3518a7dc8
david <chursa99192@gmail.com>
2022-10-25 04:31:40 +0300

    [http-server] - add github action

--- src/main/java/com/ukraine/dc/httpserver/api/exception/BadRequestException.java
+++ src/main/java/com/ukraine/dc/httpserver/api/exception/BadRequestException.java
@@ -3,7 +3,6 @@ package com.ukraine.dc.httpserver.api.exception;
 import com.ukraine.dc.httpserver.api.model.StatusCode;
 
 public class BadRequestException extends HttpException {
-
     public BadRequestException(String message) {
         super(StatusCode.BAD_REQUEST, message);
     }
